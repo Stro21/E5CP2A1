bccc0d97c9b7e52758168861dcbfde04df9274ac
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-22 14:29:57 -0300

    Se avanza con c++.

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,5 +1,69 @@
-#include <stdlib>
+#include <cstdlib>
+#include <iostream>
 
-class mano{
-	
+using namespace std;
+
+void ganador(string jugador, string computadora){
+	if (jugador == "tijera" && computadora == "tijera"){
+		cout << "El jugador empato ya que ambos eligieron " << jugador << "./n";
+	}
+	else if (jugador == "tijera" && computadora == "papel")
+	{
+		cout << "El jugador gana porque el eligió " << jugador << " y la computadora eligió " << computadora << "./n";
+	}
+	else if (jugador == "tijera" && computadora == "piedra")
+	{
+		cout << "El jugador pierde porque el eligió " << jugador << " y la computadora eligió " << computadora << "./n";
+	}
+	else if (jugador == "papel" && computadora == "papel"){
+		cout << "El jugador empato ya que ambos eligieron " << jugador << "./n";
+	}
+	else if (jugador == "papel" && computadora == "piedra")
+	{
+		cout << "El jugador gana porque el eligió " << jugador << " y la computadora eligió " << computadora << "./n";
+	}
+	else if (jugador == "papel" && computadora == "tijera")
+	{
+		cout << "El jugador pierde porque el eligió " << jugador << " y la computadora eligió " << computadora << "./n";
+	}
+	else if (jugador == "piedra" && computadora == "piedra"){
+		cout << "El jugador empato ya que ambos eligieron " << jugador << "./n";
+	}
+	else if (jugador == "piedra" && computadora == "tijera")
+	{
+		cout << "El jugador gana porque el eligió " << jugador << " y la computadora eligió " << computadora << "./n";
+	}
+	else if (jugador == "piedra" && computadora == "papel")
+	{
+		cout << "El jugador pierde porque el eligió " << jugador << " y la computadora eligió " << computadora << "./n";
+	}
+	else{
+		cout << "Error!/n";
+	}
+}
+
+string eleccion_computadora(){
+	int eleccion = rand() % 3;
+	if(eleccion == 0){
+		return "tijera";
+	}
+	else if (eleccion == 1)
+	{
+		return "papel";
+	}
+	else{
+		return "piedra";
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	string jugador, computadora;
+	computadora = eleccion_computadora();
+	do{
+		cout << "Elija piedra, papel o tijera./n";
+		cin >> jugador;
+	}while(jugador != "piedra" || jugador != "papel" || jugador != "tijera");
+
+	return 0;
 }
